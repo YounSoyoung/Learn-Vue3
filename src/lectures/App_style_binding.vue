adbb7a55ee5e27294ef45e8516172a4c5e6a176b
YounSoyoung <grace902853@gmail.com>
2023-07-09 22:29:13 +0900

    바인딩

---
+++ src/lectures/App_style_binding.vue
@@ -0,0 +1,37 @@
+<template>
+	<div>
+		<div :style="styleObject">
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni provident
+			excepturi dolorem dignissimos repudiandae aspernatur repellat eveniet,
+			cupiditate error incidunt aliquam voluptatibus, cum libero a culpa
+			accusantium neque architecto eaque.
+		</div>
+		<button v-on:click="fontSize--">-</button>
+		<button v-on:click="fontSize++">+</button>
+		<button></button>
+	</div>
+</template>
+
+<script>
+import { computed, ref } from 'vue';
+export default {
+	setup() {
+		// const styleObject = reactive({
+		// 	color: 'red',
+		// 	fontSize: '13px',
+		// });
+
+		const fontSize = ref(13);
+
+		const styleObject = computed(() => {
+			return {
+				color: 'red',
+				fontSize: fontSize.value + 'px',
+			};
+		});
+		return { styleObject, fontSize };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
