2135b09d465a30cdf5fca3513ce8eb8fb7c5eee9
YounSoyoung <grace902853@gmail.com>
2023-07-10 17:33:02 +0900

    Events

---
+++ src/components/LabelInput.vue
@@ -0,0 +1,23 @@
+<template>
+	<label>
+		이름
+		<!-- <input v-model="username" type="text" /> -->
+		<input
+			:value="modelValue"
+			@input="event => $emit('update:modelValue', event.target.value)"
+			type="text"
+		/>
+	</label>
+</template>
+
+<script>
+export default {
+	props: ['modelValue'],
+	emits: ['update:modelValue'],
+	setup() {
+		return {};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
