f3cda63daf172378f8c76648130839ee1abe3a2e
YounSoyoung <grace902853@gmail.com>
2023-07-10 15:25:44 +0900

    SFC

---
+++ src/components/AppCard.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="card">
+		<div class="card-body">
+			<h5 class="card-title red">Card title</h5>
+
+			<p class="card-text">
+				Some quick example text to build on the card title and make up the bulk
+				of the card's content.
+			</p>
+			<a href="#" class="btn btn-primary">Go somewhere</a>
+		</div>
+	</div>
+</template>
+
+<script>
+import { ref, useCssModule } from 'vue';
+
+export default {
+	setup() {
+		// const style = useCssModule();
+		// console.log('style: ', style);
+		console.log('AppCard setup()');
+
+		const color = ref('red');
+		color.value = 'blue';
+		return { color };
+	},
+};
+</script>
+
+<style>
+.red {
+	color: v-bind(color) !important;
+}
+</style>
+
+<!-- <style module="classes">
+.red {
+	color: red !important;
+}
+</style> -->
+
+<!-- scoped 속성을 적용하면 아래의 스타일은 현재 파일에서만 적용된다. -->
+<!-- <style scoped>
+.red {
+	color: red !important;
+}
+</style> -->
